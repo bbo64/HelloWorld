16ebb89bc5c5544512514f873f782fa13fc67acd
BBO <BBO@DT_DLOG_BBO.dlog.local>
2013-04-09 15:09:52 +0200

    Videotutorial

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 // Dies ist ein Superprogramm!
+//Videotutorial ist cool.
 		System.out.println("Hello, World");
         
     }
