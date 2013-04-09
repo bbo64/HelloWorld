5f7a57cf697ee115e1db6c7b625815370abe681f
BBO <BBO@DT_DLOG_BBO.dlog.local>
2013-04-09 15:59:04 +0200

    Finally

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 // Dies ist ein Superprogramm!
 //Videotutorial ist cool.
+// finally i got it....
 		System.out.println("Hello, World");
         
     }
