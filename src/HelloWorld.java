4ac4e17a5df2764fd9732d061e06eca1979423e8
BBO <BBO@DT_DLOG_BBO.dlog.local>
2013-04-10 08:02:07 +0200

    Text Printline gendert

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-// Dies ist ein Superprogramm!
-//Videotutorial ist cool.
-// finally i got it....
-//Remote aus GIT Repository ist einfacher,,,,,,
-		System.out.println("Hello, World");
+		System.out.println("Hello, Dlog");
         
     }
 }
